c1ee7d9de1e40458bfc367b421edac829b36c4e5
jeffrey <piccirilloj1@gmail.com>
2022-09-25 18:53:30 -0500

    Set up third step (further choices)

--- src/components/steps/step3.vue
+++ src/components/steps/step3.vue
@@ -2,14 +2,47 @@
   <v-stepper-content step="3">
     <v-card class="past-scores">
       <v-container class="container-override">
-        <v-card-title class="text-h5">
-          Step 2
+        <v-card-title>
+          <div>
+            <h1 class="text-h5">
+              Further choices
+            </h1>
+            <h2 class="text-h6 font-italic grey--text">
+              Description about what the investigator is choosing here
+            </h2>
+          </div>
         </v-card-title>
-        <v-card-text style="padding: 0px;" v-if="historicalData.length">
-          <Chart :historicalData="historicalData" />
-        </v-card-text>
-        <v-card-text v-else>
-          <div>no data available</div>
+        <v-card-text>
+          <div class="card-container">
+            <Card
+              identifier="1"
+              title="Reporting pairwise differences"
+              description="description for reporting pairwise differences"
+              :currentlySelected="selected"
+              @selected="selected = '1'"
+            />
+            <Card
+              identifier="2"
+              title="Reporting overall effect"
+              description="description for reporting on overall effect"
+              :currentlySelected="selected"
+              @selected="selected = '2'"
+            />
+            <Card
+              identifier="3"
+              title="Adjusted association with predictor"
+              description="descrpition for adjusted association with predictor"
+              :currentlySelected="selected"
+              @selected="selected = '3'"
+            />
+            <Card
+              identifier="4"
+              title="Overall model"
+              description="over all model card description"
+              :currentlySelected="selected"
+              @selected="selected = '4'"
+            />
+          </div>
         </v-card-text>
         <v-card-actions class="show-on-desktop">
           <div class="button-container">
@@ -30,17 +63,15 @@
 </template>
 
 <script>
+import Card from '../Card.vue';
+
 export default {
-  components: {},
+  components: { Card },
   computed: {},
-  methods: {
-    addData() {
-      console.log('Add Data');
-    },
-  },
+  methods: {},
   data() {
     return {
-      historicalData: [],
+      selected: '',
     };
   },
 };
