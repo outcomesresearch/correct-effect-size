9de2d2283fe43f4865a3efe753020fadc02d3387
jeffrey <piccirilloj1@gmail.com>
2022-12-30 15:13:31 -0600

    Add manual preview / embedded meta tags for Correct Effect Size applet

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
+const webpackParams = {
+  title: 'Choose the Correct Effect Size',
+  description:
+    'Applet to help clinical study investigators select the appropriate effect size measure for planning and reporting the results of their research.',
+  site_name:
+    'Applets from Clinical Outcomes Research at Washington University in St Louis',
+  url: 'https://outcomesresearch.github.io/correct-effect-size/',
+};
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/correct-effect-size/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/correct-effect-size/' : '/',
   transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      Object.entries(webpackParams).forEach(([key, value]) => {
+        args[0][key] = value;
+      });
+      return args;
+    });
+  },
 };
